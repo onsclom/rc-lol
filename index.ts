233e8f9008aeca42b9b03ff1fdc28d1a86d67c91
Austin Merrick <austinmerrick@onsclom.net>
2026-04-07 09:32:09 -0700

    .

--- index.ts
+++ index.ts
@@ -1,12 +1,38 @@
+import index from "./index.html";
+
+// rc cal -> calendar
+// fallback -> google
+
 Bun.serve({
   port: 3000,
-  fetch(req) {
-    const queryParams = new URL(req.url).searchParams;
+  routes: {
+    "/frontend": index,
 
-    console.log(queryParams);
+    "/": {
+      async GET(req) {
+        const queryParams = new URL(req.url).searchParams;
+        console.log(queryParams);
 
-    return new Response("Hello via Bun!!");
+        const qParam = queryParams.get("q");
+        if (!qParam) {
+          return Response.json({});
+        }
+
+        if (qParam === "cal") {
+          // redirect to https://www.recurse.com/calendar
+          return Response.redirect("https://www.recurse.com/calendar", 302);
+        } else {
+          // fallback should use google
+          return Response.redirect(
+            `https://www.google.com/search?q=${qParam}`,
+            302,
+          );
+        }
+      },
+    },
   },
 });
+
+// rc next event
 
 console.log("Server running on http://localhost:3000");
