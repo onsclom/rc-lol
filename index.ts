351897d075604a2a3de5ffd8acccdb4a92e5dce2
Shreyash Saitwal <40118018+shreyashsaitwal@users.noreply.github.com>
2026-04-07 23:16:36 +0530

    chore: clean stuff up a bit

--- index.ts
+++ index.ts
@@ -1,38 +1,50 @@
-import index from "./index.html";
+interface Command {
+  keyword: string;
+  handler: (args?: string[]) => string;
+}
 
-// rc cal -> calendar
-// fallback -> google
+const registry: Command[] = [
+  {
+    keyword: "cal",
+    handler: () => "https://www.recurse.com/calendar",
+  },
+  {
+    keyword: "z",
+    handler: (args) => {
+      if (!args || args?.length == 0) {
+        return "https://recurse.zulipchat.com";
+      }
+      return `https://recurse.zulipchat.com/#narrow/search/${encodeURIComponent(args.join(" "))}`;
+    },
+  },
+];
 
 Bun.serve({
   port: 3000,
   routes: {
-    "/frontend": index,
-
     "/": {
       async GET(req) {
         const queryParams = new URL(req.url).searchParams;
-        console.log(queryParams);
+        const query = queryParams.get("q");
 
-        const qParam = queryParams.get("q");
-        if (!qParam) {
+        if (!query) {
           return Response.json({});
         }
 
-        if (qParam === "cal") {
-          // redirect to https://www.recurse.com/calendar
-          return Response.redirect("https://www.recurse.com/calendar", 302);
-        } else {
-          // fallback should use google
-          return Response.redirect(
-            `https://www.google.com/search?q=${qParam}`,
-            302,
-          );
+        const [keyword, ...args] = query.split(" ");
+
+        const cmd = registry.find((el) => el.keyword == keyword);
+        if (cmd) {
+          return Response.redirect(cmd.handler(args), 302);
         }
+
+        return Response.redirect(
+          `https://www.google.com/search?q=${encodeURIComponent(query)}`,
+          302,
+        );
       },
     },
   },
 });
-
-// rc next event
 
 console.log("Server running on http://localhost:3000");
